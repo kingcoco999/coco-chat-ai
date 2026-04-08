42e919fe9655e40b65fe5c5b7074d2a461814e17
kingcoco999 <like_cooc@163.com>
2026-04-09 00:51:49 +0800

    Create capacitor.config.js

---
+++ capacitor.config.js
@@ -0,0 +1,38 @@
+const config = {
+  appId: 'com.coco.chatai',
+  appName: 'Coco Chat AI',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https',
+    hostname: 'app.coco.chatai',
+    allowNavigation: [
+      'https://api.deepseek.com',
+      'https://api.moonshot.cn',
+      'https://api.openai.com',
+      'https://generativelanguage.googleapis.com',
+      'https://open.bigmodel.cn',
+      'https://dashscope.aliyuncs.com'
+    ]
+  },
+  android: {
+    allowMixedContent: true,
+    captureInput: true,
+    webViewLimitsNavigations: true,
+    backgroundColor: '#fafaf8'
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 0
+    },
+    StatusBar: {
+      style: 'light',
+      backgroundColor: '#fafaf8'
+    },
+    Keyboard: {
+      resize: 'body',
+      resizeOnFullScreen: true
+    }
+  }
+};
+
+module.exports = config;
